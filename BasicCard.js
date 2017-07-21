ee08aa3cfdaad766179543e3a599bbb754103e94
francisco <francisco.deppe@gmail.com>
2017-07-21 13:42:53 -0700

    first commits

---
+++ BasicCard.js
@@ -0,0 +1,16 @@
+var BasicCard = function(front, back) {
+	this.front = front;
+	this.back = back;
+}
+
+var firstPresident = new BasicCard(
+	"Who was the first President of the United States?", "George Washington"
+)
+
+
+
+
+
+
+
+module.exports = BasicCard;
